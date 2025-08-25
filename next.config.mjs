95e7bfc66ded1d77fcd5b12cf11d1ae0775b9ac5
Your Name <you@example.com>
2025-08-25 17:31:56 +0530

    fix: configure standalone output for Netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // enables static export for GitHub Pages
-  basePath: '/blog-frontend', // repo name
-  assetPrefix: '/blog-frontend/',
-
+  output: 'standalone', // <-- ADD THIS LINE
   images: {
-    unoptimized: true, // GitHub Pages doesn’t support Image Optimization
     remotePatterns: [
       {
         protocol: 'https',
